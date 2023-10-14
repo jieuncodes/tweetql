d944381d4a72c1a44d2c46ce41aaa54d80c7b6b2
youredith <110220308+jieuncodes@users.noreply.github.com>
2023-10-15 06:44:29 +0900

    feat: type resolver

--- server.js
+++ server.js
@@ -1,9 +1,37 @@
 import { ApolloServer, gql } from "apollo-server";
 
+let tweets = [
+  {
+    id: "1",
+    text: "first one!",
+    userId: "2",
+  },
+  {
+    id: "2",
+    text: "second one",
+    userId: "1",
+  },
+];
+
+let users = [
+  {
+    id: "1",
+    firstName: "barbie",
+    lastName: "doll",
+  },
+  {
+    id: "2",
+    firstName: "Elon",
+    lastName: "Musk",
+  },
+];
+
 const typeDefs = gql`
   type User {
-    id: ID
-    username: String
+    id: ID!
+    firstName: String!
+    lastName: String!
+    fullName: String!
   }
   type Tweet {
     id: ID!
@@ -11,30 +39,54 @@ const typeDefs = gql`
     author: User
   }
   type Query {
-    allTweets: [Tweet]
-    tweet(id: ID): Tweet
+    allUsers: [User!]!
+    allTweets: [Tweet!]!
+    tweet(id: ID!): Tweet
   }
   type Mutation {
-    postTweet(text: String, userId: ID): Tweet
-    deleteTweet(id: ID): Boolean
+    postTweet(text: String!, userId: ID!): Tweet!
+    deleteTweet(id: ID!): Boolean!
   }
 `;
-const tweets = [
-  {
-    id: 1,
-    text: "Hello World",
-  },
-  {
-    id: 2,
-    text: "Bye World",
-  },
-];
 
 const resolvers = {
   Query: {
-    allTweets: () => tweets,
+    allTweets() {
+      return tweets;
+    },
     tweet(root, { id }) {
-      return tweets.find((tweet) => tweet.id === Number(id));
+      return tweets.find((tweet) => tweet.id === id);
+    },
+    allUsers() {
+      console.log("allUsers called!");
+      return users;
+    },
+  },
+  Mutation: {
+    postTweet(_, { text, userId }) {
+      const newTweet = {
+        id: tweets.length + 1,
+        text,
+        userId,
+      };
+      tweets.push(newTweet);
+      return newTweet;
+    },
+    deleteTweet(_, { id }) {
+      const tweet = tweets.find((tweet) => tweet.id === id);
+      if (!tweet) return false;
+      tweets = tweets.filter((tweet) => tweet.id !== id);
+      return true;
+    },
+  },
+  User: {
+    fullName({ firstName, lastName }) {
+      return `${firstName} ${lastName}`;
+    },
+  },
+  Tweet: {
+    author({ userId }) {
+      return users.find((user) => user.id === userId);
     },
   },
 };
